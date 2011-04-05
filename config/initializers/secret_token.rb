455dd173fa1b0470ef5a31b5a2bf945b0e82311e
Diego Lucena <diegojlucena@gmail.com>
2011-04-05 20:08:30 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Adventureworks4rails::Application.config.secret_token = '627c7da3b9a93894cd8c7363f2526e9ef46761fa8556b5c7a3060620897950c5af5001e14509ac0e7e2c52938258e6d37f2f2f3f4a6bfbd13f7814a0303750b2'
